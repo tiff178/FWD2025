ddb528e1cad2614fe83c8c54259eb1ff8cab0996
tiff178 <tiffmt817@gmail.com>
2025-03-27 22:14:24 -0700

    add comments to code

--- webdev/portfolio.js
+++ webdev/portfolio.js
@@ -1,18 +1,33 @@
-// array of fun facts to be shown when the button is clicked
-const funFacts = [
-    "sheCodes was established in 2015!",
-    "sheCodes is a part of the ACM-W or Association for Computing Machinery's Council on Women in Computing Chapter.",
-    "sheCodes' values are community, outreach, and growth!",
-    "The official formatting of sheCodes is just “sheCodes” with the lowercase “s” at the beginning and the capital “C”. Even if sheCodes is at the beginning of the sentence, the “s” is still lowercase.",
-    "This year is sheCodes second year back in-person for FWD since 2019! "
-];
+/**
+ * Function: display_facts()
+ * This function does not take in any parameters and will update the html file in return
+ * 
+ * It creates an array, funFacts, that holds the facts about sheCodes.
+ * 
+ * Then it creates a variable, rand_facts, that will store a random element of that array.
+ * 
+ * Logic:
+ * 1. Use Math.random() to return a random number between 0 - 1
+ * 2. Multiply that number by the length of the array: (Math.random() * funFacts.length)
+ * 3. Use Math.floor() to round that number down: (Math.floor(Math.random() * funFacts.length))
+ * 4. Use that value to get a random element of the array: funFacts[(Math.floor(Math.random() * funFacts.length))]
+ * 5. Store the random element into the variable: const rand_facts = funFacts[(Math.floor(Math.random() * funFacts.length))];
+ * 
+ * Last it updates the html by getting the element by finding its matching id and updating it accordingly:
+ * document.getElementById("facts").innerHTML = rand_facts;
+ * This means it is finding the element in the html file with the id "facts" and updating it to the rand_facts text
+ */
 
-// get button and display elements
-const button = document.getElementById('funFactButton');
-const factDisplay = document.getElementById('factDisplay');
+function display_facts() {
+    const funFacts = [
+        "sheCodes was established in 2015!",
+        "sheCodes is a part of the ACM-W or Association for Computing Machinery's Council on Women in Computing Chapter.",
+        "sheCodes' values are community, outreach, and growth!",
+        "The official formatting of sheCodes is just “sheCodes” with the lowercase “s” at the beginning and the capital “C”.",
+        "This year is sheCodes second year back in-person for FWD since 2019! "
+    ];
 
-// set up a function to display a random fun fact from the array
-button.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * funFacts.length);
-    factDisplay.textContent = funFacts[randomIndex];
-});
+    const rand_facts = funFacts[(Math.floor(Math.random() * funFacts.length))];
+    document.getElementById("facts").innerHTML = rand_facts
+
+}
